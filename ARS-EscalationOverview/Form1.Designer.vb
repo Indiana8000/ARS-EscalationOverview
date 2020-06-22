c88cf038d82d23818a97ba4e9fcaa276e914efbf
Andreas Kreisl <indiana@bluepaw.de>
2020-06-22 08:51:55 +0200

    Projektdateien hinzufügen.

---
+++ ARS-EscalationOverview/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.btn_disconnect = New System.Windows.Forms.Button()
+        Me.btn_Connect = New System.Windows.Forms.Button()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.txt_Password = New System.Windows.Forms.TextBox()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.txt_Username = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.txt_Port = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.txt_Server = New System.Windows.Forms.TextBox()
+        Me.btn_exec = New System.Windows.Forms.Button()
+        Me.lst_Pools = New System.Windows.Forms.ListBox()
+        Me.lst_escal = New System.Windows.Forms.ListView()
+        Me.col_Name = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.col_Interval = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.col_countAll = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.col_countMatch = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'Label18
+        '
+        Me.Label18.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label18.Location = New System.Drawing.Point(12, 47)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(840, 2)
+        Me.Label18.TabIndex = 74
+        '
+        'btn_disconnect
+        '
+        Me.btn_disconnect.Enabled = False
+        Me.btn_disconnect.Location = New System.Drawing.Point(777, 21)
+        Me.btn_disconnect.Name = "btn_disconnect"
+        Me.btn_disconnect.Size = New System.Drawing.Size(75, 20)
+        Me.btn_disconnect.TabIndex = 73
+        Me.btn_disconnect.Text = "Logout"
+        Me.btn_disconnect.UseVisualStyleBackColor = True
+        '
+        'btn_Connect
+        '
+        Me.btn_Connect.Location = New System.Drawing.Point(696, 21)
+        Me.btn_Connect.Name = "btn_Connect"
+        Me.btn_Connect.Size = New System.Drawing.Size(75, 20)
+        Me.btn_Connect.TabIndex = 72
+        Me.btn_Connect.Text = "Login"
+        Me.btn_Connect.UseVisualStyleBackColor = True
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(492, 5)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(53, 13)
+        Me.Label17.TabIndex = 71
+        Me.Label17.Text = "Password"
+        '
+        'txt_Password
+        '
+        Me.txt_Password.Location = New System.Drawing.Point(490, 21)
+        Me.txt_Password.Name = "txt_Password"
+        Me.txt_Password.Size = New System.Drawing.Size(200, 20)
+        Me.txt_Password.TabIndex = 70
+        Me.txt_Password.UseSystemPasswordChar = True
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(286, 5)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(55, 13)
+        Me.Label16.TabIndex = 69
+        Me.Label16.Text = "Username"
+        '
+        'txt_Username
+        '
+        Me.txt_Username.Location = New System.Drawing.Point(284, 21)
+        Me.txt_Username.Name = "txt_Username"
+        Me.txt_Username.Size = New System.Drawing.Size(200, 20)
+        Me.txt_Username.TabIndex = 68
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(220, 5)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(26, 13)
+        Me.Label15.TabIndex = 67
+        Me.Label15.Text = "Port"
+        '
+        'txt_Port
+        '
+        Me.txt_Port.Location = New System.Drawing.Point(218, 21)
+        Me.txt_Port.Name = "txt_Port"
+        Me.txt_Port.Size = New System.Drawing.Size(60, 20)
+        Me.txt_Port.TabIndex = 66
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(14, 5)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(38, 13)
+        Me.Label14.TabIndex = 65
+        Me.Label14.Text = "Server"
+        '
+        'txt_Server
+        '
+        Me.txt_Server.Location = New System.Drawing.Point(12, 21)
+        Me.txt_Server.Name = "txt_Server"
+        Me.txt_Server.Size = New System.Drawing.Size(200, 20)
+        Me.txt_Server.TabIndex = 64
+        '
+        'btn_exec
+        '
+        Me.btn_exec.Enabled = False
+        Me.btn_exec.Location = New System.Drawing.Point(12, 55)
+        Me.btn_exec.Name = "btn_exec"
+        Me.btn_exec.Size = New System.Drawing.Size(75, 20)
+        Me.btn_exec.TabIndex = 75
+        Me.btn_exec.Text = "Run"
+        Me.btn_exec.UseVisualStyleBackColor = True
+        '
+        'lst_Pools
+        '
+        Me.lst_Pools.FormattingEnabled = True
+        Me.lst_Pools.Location = New System.Drawing.Point(12, 81)
+        Me.lst_Pools.Name = "lst_Pools"
+        Me.lst_Pools.Size = New System.Drawing.Size(75, 290)
+        Me.lst_Pools.TabIndex = 76
+        '
+        'lst_escal
+        '
+        Me.lst_escal.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.col_Name, Me.col_Interval, Me.col_countAll, Me.col_countMatch})
+        Me.lst_escal.Location = New System.Drawing.Point(93, 55)
+        Me.lst_escal.Name = "lst_escal"
+        Me.lst_escal.Size = New System.Drawing.Size(759, 316)
+        Me.lst_escal.TabIndex = 77
+        Me.lst_escal.UseCompatibleStateImageBehavior = False
+        Me.lst_escal.View = System.Windows.Forms.View.Details
+        '
+        'col_Name
+        '
+        Me.col_Name.Text = "Name"
+        Me.col_Name.Width = 200
+        '
+        'col_Interval
+        '
+        Me.col_Interval.Text = "Interval"
+        Me.col_Interval.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'col_countAll
+        '
+        Me.col_countAll.Text = "Count All"
+        Me.col_countAll.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'col_countMatch
+        '
+        Me.col_countMatch.Text = "Count Match"
+        Me.col_countMatch.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(864, 384)
+        Me.Controls.Add(Me.lst_escal)
+        Me.Controls.Add(Me.lst_Pools)
+        Me.Controls.Add(Me.btn_exec)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.btn_disconnect)
+        Me.Controls.Add(Me.btn_Connect)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.txt_Password)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.txt_Username)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.txt_Port)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.txt_Server)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "ARS-EscalationOverview"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label18 As Label
+    Friend WithEvents btn_disconnect As Button
+    Friend WithEvents btn_Connect As Button
+    Friend WithEvents Label17 As Label
+    Friend WithEvents txt_Password As TextBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents txt_Username As TextBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents txt_Port As TextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents txt_Server As TextBox
+    Friend WithEvents btn_exec As Button
+    Friend WithEvents lst_Pools As ListBox
+    Friend WithEvents lst_escal As ListView
+    Friend WithEvents col_Name As ColumnHeader
+    Friend WithEvents col_Interval As ColumnHeader
+    Friend WithEvents col_countAll As ColumnHeader
+    Friend WithEvents col_countMatch As ColumnHeader
+End Class
---
+++ ARS-EscalationOverview/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+    Dim oServer As BMC.ARSystem.Server
+    Dim tEscalations As DataTable
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Date.Now.Year > 2020 Or Date.Now.Month > 9 Then
+            MsgBox("Trial Period Expired! Please contact the Developer.", vbExclamation, "Unregistered Version")
+            Application.Exit()
+        End If
+
+        oServer = New BMC.ARSystem.Server
+
+        txt_Server.Text = My.Settings.Server
+        txt_Port.Text = My.Settings.Port
+        txt_Username.Text = My.Settings.Username
+        If txt_Server.Text.Length > 0 Then
+            txt_Password.Select()
+        End If
+
+        tEscalations = New DataTable
+        tEscalations.Columns.Add("Name", GetType(String))
+        tEscalations.Columns.Add("Enabled", GetType(Boolean))
+        tEscalations.Columns.Add("Pool", GetType(Integer))
+        tEscalations.Columns.Add("Interval", GetType(Integer))
+        tEscalations.Columns.Add("countAll", GetType(Integer))
+        tEscalations.Columns.Add("countMatched", GetType(Integer))
+    End Sub
+
+    Private Sub btn_Connect_Click(sender As Object, e As EventArgs) Handles btn_Connect.Click
+        Dim alForms As ArrayList
+
+        My.Settings.Server = txt_Server.Text
+        My.Settings.Port = txt_Port.Text
+        My.Settings.Username = txt_Username.Text
+        My.Settings.Save()
+
+        Try
+            oServer.Login(txt_Server.Text, txt_Username.Text, txt_Password.Text, "", "de_DE", "UTF-8", txt_Port.Text)
+            alForms = oServer.GetListForm()
+            btn_Connect.Enabled = False
+            btn_disconnect.Enabled = True
+            btn_exec.Enabled = True
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btn_disconnect_Click(sender As Object, e As EventArgs) Handles btn_disconnect.Click
+        oServer.Logout()
+        btn_Connect.Enabled = True
+        btn_disconnect.Enabled = False
+        btn_exec.Enabled = False
+    End Sub
+
+    Private Sub txt_Password_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt_Password.KeyPress
+        If txt_Password.Text.Length > 0 And Asc(e.KeyChar) = 13 Then
+            btn_Connect.Select()
+            Application.DoEvents()
+            btn_Connect_Click(sender, New EventArgs())
+        End If
+    End Sub
+
+    Private Sub btn_exec_Click(sender As Object, e As EventArgs) Handles btn_exec.Click
+        Dim es As ArrayList
+        Dim ecl As BMC.ARSystem.Escalation
+        Dim ti As BMC.ARSystem.EscalationInterval
+        Dim tt As BMC.ARSystem.EscalationTime
+        Dim de As DictionaryEntry
+
+        Dim pool As Integer
+        Dim interval As Integer
+
+        btn_exec.Enabled = False
+        es = oServer.GetListEscalation()
+        For Each s As String In es
+            ecl = oServer.GetEscalation(s)
+
+            pool = 1
+            If ecl.Properties.Contains(60024) Then
+                Try
+                    pool = ecl.Properties(60024)
+                Catch ex As Exception
+                    'TBD
+                End Try
+            End If
+
+            While lst_Pools.Items.Count < pool
+                lst_Pools.Items.Add("Pool " & (lst_Pools.Items.Count + 1))
+                Application.DoEvents()
+            End While
+
+            If ecl.TimeCriteria.GetType Is GetType(BMC.ARSystem.EscalationInterval) Then
+                ti = ecl.TimeCriteria
+                interval = ti.Minutes
+                tEscalations.Rows.Add(s, ecl.Enabled, pool, interval, 0, 0)
+            Else
+                tt = ecl.TimeCriteria
+                'TBD
+            End If
+        Next
+        btn_exec.Enabled = True
+    End Sub
+
+    Private Sub lst_Pools_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lst_Pools.SelectedIndexChanged
+        Dim rows As DataRow()
+        Dim EscalItem As ListViewItem
+
+        lst_escal.Items.Clear()
+        rows = tEscalations.Select("Pool = " & lst_Pools.SelectedIndex, "Interval ASC, Name ASC")
+        For Each row As DataRow In rows
+            Console.WriteLine("DEBUG: " & row("Name"))
+            EscalItem = New ListViewItem
+
+            EscalItem.Text = row("Name")
+            EscalItem.SubItems.Add(row("Interval"))
+            EscalItem.SubItems.Add(row("countAll"))
+            EscalItem.SubItems.Add(row("countMatched"))
+
+            If row("Enabled") = False Then
+                EscalItem.ForeColor = Color.Silver
+            End If
+
+            lst_escal.Items.Add(EscalItem)
+        Next
+
+        col_Name.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+        col_Interval.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+        col_countAll.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+        col_countMatch.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent)
+    End Sub
+End Class
